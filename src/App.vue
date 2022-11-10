874491139e9f72652f0f741190c9a09dff4b074a
JCorpse <24427007+JCorpse@users.noreply.github.com>
2022-11-10 18:00:21 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <OnonClicker/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import OnonClicker from './components/OnonClicker.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    OnonClicker
   }
 }
 </script>
