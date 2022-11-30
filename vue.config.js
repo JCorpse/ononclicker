c718b63e1e46379880553c889debb727914f2ee7
JCorpse <24427007+JCorpse@users.noreply.github.com>
2022-11-30 15:25:01 +0800

    test

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const {defineConfig} = require('@vue/cli-service')
 module.exports = defineConfig({
+    publicPath:"/ononclicker",
     transpileDependencies: true,
     configureWebpack: {
         performance: {
